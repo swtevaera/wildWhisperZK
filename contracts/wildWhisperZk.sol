aee2edc9e83d3e95b8ac193e3615609f11117843
Sarthak Walia <sarthak.19062@iitgoa.ac.in>
2024-06-24 20:23:21 +0530

    modified wildWhisperZK Contract

--- contracts/wildWhisperZk.sol
+++ contracts/wildWhisperZk.sol
@@ -1,35 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import "./impostorVerifier.sol";
 import "./killProofVerifier.sol";
 import "./sabotageVerifier.sol";
 import "./taskVerifier.sol";
 contract WildWhisperZk {
-    impostorVerifier public immutable _impostorVerifier;
+    error ProofAlreadyUsed();
     killProofVerifier public immutable _killProofVerifier;
     sabotageVerifier public immutable _sabotageVerifier;
     taskVerifier public immutable _taskVerifier;
-    constructor(address __impostorVerifier, address __killProofVerifier, address __sabotageVerifier, address __taskVerifier) {
-        _impostorVerifier = impostorVerifier(__impostorVerifier);
+
+    mapping(bytes32 => bool) public isAlreadyVerifiedProof;
+
+    constructor(address __killProofVerifier, address __sabotageVerifier, address __taskVerifier) {
         _killProofVerifier = killProofVerifier(__killProofVerifier);
         _sabotageVerifier = sabotageVerifier(__sabotageVerifier);
         _taskVerifier = taskVerifier(__taskVerifier);
     }
-    function verifyImpostorProof(
-        uint[2] memory a,
-        uint[2][2] memory b,
-        uint[2] memory c,
-        uint[1] memory input
-    ) public view returns (bool) {
-        return _impostorVerifier.verifyProof(a, b, c, input);
-    }
+
     function verifyKillProof(
         uint[2] memory a,
         uint[2][2] memory b,
         uint[2] memory c,
         uint[1] memory input
     ) public view returns (bool) {
-        return _killProofVerifier.verifyProof(a, b, c, input);
+        bytes32 proofHash = keccak256(abi.encodePacked(a, b, c, input));
+        if(verifiedProofs[proofHash]) revert ProofAlreadyUsed();
+        bool isValid = _killProofVerifier.verifyProof(a, b, c, input);
+        if(isValid) verifiedProofs[proofHash] = true;
+        return isValid;
     }
     function verifySabotageStartProof(
         uint[2] memory a,
@@ -37,7 +35,11 @@ contract WildWhisperZk {
         uint[2] memory c,
         uint[1] memory input
     ) public view returns (bool) {
-        return _sabotageVerifier.verifyProof(a, b, c, input);
+        bytes32 proofHash = keccak256(abi.encodePacked(a, b, c, input));
+        if(verifiedProofs[proofHash]) revert ProofAlreadyUsed();
+        bool isValid = _sabotageVerifier.verifyProof(a, b, c, input);
+        if(isValid) verifiedProofs[proofHash] = true;
+        return isValid;
     }
     function verifyTaskProof(
         uint[2] memory a,
@@ -45,6 +47,10 @@ contract WildWhisperZk {
         uint[2] memory c,
         uint[1] memory input
     ) public view returns (bool) {
-        return _taskVerifier.verifyProof(a, b, c, input);
+        bytes32 proofHash = keccak256(abi.encodePacked(a, b, c, input));
+        if(verifiedProofs[proofHash]) revert ProofAlreadyUsed();
+        bool isValid =  _taskVerifier.verifyProof(a, b, c, input);
+        if(isValid) verifiedProofs[proofHash] = true;
+        return isValid;
     }
 }
